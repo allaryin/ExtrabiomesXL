20714940f8bd136a217ea2f1aee590c5c45b4bc4
Scott Killen <scott@scottkillen.com>
2012-10-14 01:11:47 -0400

    Added ItemClump.java

---
+++ extrabiomes/src/extrabiomes/module/cautia/ItemClump.java
@@ -0,0 +1,17 @@
+/**
+ * This work is licensed under the Creative Commons
+ * Attribution-ShareAlike 3.0 Unported License. To view a copy of this
+ * license, visit http://creativecommons.org/licenses/by-sa/3.0/.
+ */
+
+package extrabiomes.module.cautia;
+
+import net.minecraft.src.CreativeTabs;
+import net.minecraft.src.Item;
+
+public class ItemClump extends Item {
+	public ItemClump(int par1) {
+		super(par1);
+		setCreativeTab(CreativeTabs.tabMaterials);
+	}
+}
